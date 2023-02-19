7b880347a892b912b7cd4209ef91346b08266361
lalit-8 <sandbhorlalit@gmail.com>
2023-02-19 23:58:35 +0530

    Update files

---
+++ quote-generator/js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+const button = document.querySelector("button");
+const quote = document.querySelector("blockquote p");
+const cite = document.querySelector("blockquote cite");
+
+async function updateQuote() {
+	const response = await fetch("https://api.quotable.io/random");
+	const data = await response.json();
+	if (response.ok) {
+	  quote.textContent = data.content;
+	  cite.textContent = data.author;
+	} else {
+	  quote.textContent = "An error occured";
+	  console.log(data);
+	}
+}
+
+button.addEventListener("click", updateQuote);
+updateQuote();
+});
