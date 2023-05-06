4f9ec4a3b40a9f6bda1a19e9ac26c63ec88a1918
lalit-8 <sandbhorlalit@gmail.com>
2023-05-06 23:10:17 +0530

    Update JavaScript

--- counter-on-scroll/js/main.js
+++ counter-on-scroll/js/main.js
@@ -1,1 +1,23 @@
-// JavaScript Code here
+window.addEventListener('scroll', function() {
+  var counter = document.querySelector('#counter');
+  var oTop = counter.getBoundingClientRect().top - window.innerHeight;
+  
+  if (window.pageYOffset > oTop) {
+    var counterValues = document.querySelectorAll('.counter-value');
+    counterValues.forEach(function(counterValue) {
+      var countTo = counterValue.getAttribute('data-count');
+      var countNum = parseInt(counterValue.innerText);
+      var animationDuration = 1000;
+      var increment = Math.ceil(countTo / (animationDuration / 20));
+      
+      var countAnimation = setInterval(function() {
+        countNum += increment;
+        counterValue.innerText = countNum;
+        if (countNum >= countTo) {
+          clearInterval(countAnimation);
+          counterValue.innerText = countTo;
+        }
+      }, 20);
+    });
+  }
+});
